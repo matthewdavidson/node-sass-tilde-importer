e1b3831787f0765ded33c9ce1d1f86b0b139ba80
Adam Pelle <adam.pelle@skyscanner.net>
2017-04-04 15:50:49 +0100

    fallback to resolve flat dep.

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 var path = require('path');
+var fs = require('fs');
 var findParentDir = require('find-parent-dir');
+
+/**
+ * Resolves the url in the nearest node_module directory
+ * @param {String} targetUrl url to resolve
+ * @param {String} sourceFile file which is referring to the targetUrl
+ * @returns file path or false if the file does not exist
+ */
+function resolveDeep(targetUrl, sourceFile) {
+  var packageRoot = findParentDir.sync(sourceFile, 'node_modules') || __dirname;
+  var filePath = path.resolve(packageRoot, 'node_modules', targetUrl);
+
+  return fs.existsSync(path.dirname(filePath)) && filePath;
+}
+
+/**
+ * Resolves the url in the top level node_module directory
+ * @param {String} targetUrl url to resolve
+ */
+function resolveFlat(targetUrl) {
+  return path.resolve('node_modules', targetUrl);
+}
 
 module.exports = function importer (url, prev, done) {
   if (url[ 0 ] === '~') {
-    var packageRoot = findParentDir.sync(prev, 'node_modules') || __dirname;
-    url = path.resolve(packageRoot, 'node_modules', url.substr(1));
+    var targetUrl = url.substr(1);
+    url = resolveDeep(targetUrl, prev) || resolveFlat(targetUrl);
   }
 
   return { file: url };
