b707c5c102b6d0d684700c573c6a97cc47577a2a
Adam Pelle <adam.pelle@skyscanner.net>
2017-04-05 23:47:21 +0100

    remove jsdoc comments, extend tests

--- index.js
+++ index.js
@@ -2,16 +2,11 @@ var path = require('path');
 var fs = require('fs');
 var findParentDir = require('find-parent-dir');
 
-/**
- * Resolves the url in the top level node_module directory
- * @param {String} targetUrl url to resolve
- * @param {String} source file or directory which is the base point resolving targetUrl
- */
 function resolve(targetUrl, source) {
   var packageRoot = findParentDir.sync(source, 'node_modules');
 
   if (!packageRoot) {
-    return null; // file could not be resolved as npm package
+    return null;
   }
 
   var filePath = path.resolve(packageRoot, 'node_modules', targetUrl);
@@ -26,5 +21,5 @@ module.exports = function importer (url, prev, done) {
     url = resolve(url.substr(1), prev);
   }
 
-  return url && { file: url };
+  return { file: url };
 };
